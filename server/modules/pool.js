ec95e609294e4b4b43daf48a8b7af475fdbfcb6f
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-03-10 01:44:11 -0600

    added try catch to pool

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -13,27 +13,37 @@ let config = {};
 if (process.env.DATABASE_URL) {
   // Heroku gives a url, not a connection object 
   // https://github.com/brianc/node-pg-pool
-  const params = url.parse(process.env.DATABASE_URL);
-  const auth = params.auth.split(':');
-
-  config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: { rejectUnauthorized: false },
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-  };
+  try {
+    const params = url.parse(process.env.DATABASE_URL);
+    const auth = params.auth.split(':');
+  
+    config = {
+      user: auth[0],
+      password: auth[1],
+      host: params.hostname,
+      port: params.port,
+      database: params.pathname.split('/')[1],
+      ssl: { rejectUnauthorized: false },
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+    };   
+    console.log('Connected to remote database');
+  } catch (error) {
+    console.log('pool error:', error);
+  }
 } else {
-  config = {
-    host: 'localhost', // Server hosting the postgres database
-    port: 5432, // env var: PGPORT
-    database: 'client_project', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
-    max: 10, // max number of clients in the pool
-    idleTimeoutMillis: 10000, // how long a client is allowed to remain idle before being closed
-  };
+  try {
+    config = {
+      host: 'localhost', // Server hosting the postgres database
+      port: 5432, // env var: PGPORT
+      database: 'client_project', // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+      max: 10, // max number of clients in the pool
+      idleTimeoutMillis: 10000, // how long a client is allowed to remain idle before being closed
+    };
+    console.log('Connected to localhost database'); 
+  } catch (error) {
+    console.log('pool error:', error);
+  }
 }
 
 // this creates the pool that will be shared by all other modules
